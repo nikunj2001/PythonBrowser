f0ea88859615260996b78bb88003ff8c818b26ca
nikunj gupta <59887608+nikunj2001@users.noreply.github.com>
2021-07-30 14:29:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 from PyQt5.QtWidgets  import *
+from PyQt5.QtWebEngineWidgets  import *
+from PyQt5.QtCore import *
 import sys
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super(MainWindow,self).__init__()
+        self.browser=QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
         self.showMaximized()
 
+
+#         NAv bar
+        navbar=QToolBar()
+        self.addToolBar(navbar)
+        back_button = QAction('Back',self)
+        back_button.triggered.connect((self.browser.back))
+        navbar.addAction(back_button)
+        forward_button = QAction('Forward',self)
+        forward_button.triggered.connect((self.browser.forward))
+        navbar.addAction(forward_button)
+        reload_button = QAction('Reload',self)
+        reload_button.triggered.connect((self.browser.reload))
+        navbar.addAction(reload_button)
+        home_button = QAction("Home",self);
+        home_button.triggered.connect(self.navigate_home)
+        navbar.addAction(home_button)
+        self.url_bar = QLineEdit()
+        navbar.addWidget(self.url_bar)
+        self.browser.urlChanged.connect(self.update_url)
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('http://google.com'))
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self,q):
+        self.url_bar.setText(q.toString())
 
 
 app = QApplication(sys.argv)
